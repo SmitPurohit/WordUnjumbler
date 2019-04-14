abf5497c8cf74ab29dacc5f93e4e335912078ce6
SmitPurohit <35670650+SmitPurohit@users.noreply.github.com>
2019-04-13 21:17:47 -0500

    Added conditions firstLetter and countVowels
    These two methods allow us to only add certain words to the array of all words

--- Dejumbler.java
+++ Dejumbler.java
@@ -5,46 +5,75 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Dejumbler
 {
-   static ArrayList<String> listWords = new ArrayList<String>();
+   static ArrayList<String> listWords = new ArrayList<String>(); //array of all words in English
+   static ArrayList<String> array = new ArrayList<String>(); //array of letters in input word
+   static ArrayList<String> exclude = new ArrayList<String>(); //array of already created combinations from jumble
+   static int count = 0;
+   static  String word;
    public static void main(String args[])
    {
       Scanner input = new Scanner(System.in);
-       System.out.print("Enter jumbled word: ");
+      System.out.print("Enter jumbled word: ");
       
-      String word = input.nextLine(); 
+      word = input.nextLine(); 
+      
+      //convert String into String array to allow for easier manipulation
+      int length = word.length();
+      for(int k = 0; k < length; k++)
+      {
+         if(k==length-1)
+            array.add(word.substring(k));
+         else
+            array.add(word.substring(k,k+1));
+         
+      }
+      
       fillList();
-      System.out.println(solve(word.toLowerCase()));
+      
+      System.out.println(solve(word.toLowerCase()) + "\nSolved in: " + count +" iterations");
    }
-   //fills the arraylist of all words in english dictionary --> maybe only add words that have a first letter
+   
+   
+   //fills the arraylist of words from the english dictionary whose first letter is in the array listWords and if the length is the same
+   //in addition it only adds to the array if the word and the input have the same amount of vowels
+   //all conditions are to reduce the amount of words in the list and therefore less time to iterate through it
+   
+   //as far as I can tell fillList takes significantly less time so complexity here would not affect time as much compared to jumble
    public static void fillList()
    {
       try{
          BufferedReader br = new BufferedReader(new FileReader("words_alpha.txt"));
          String line;
          while((line = br.readLine()) != null) {
-            listWords.add(line);
+            for(String letter: array)
+            {
+               if(getFirstLetter(line).equals(letter)&&word.length()==line.length()&&countVowels(line)==countVowels(word))
+               {
+                  listWords.add(line);
+                  break;
+               }
+            }
+            
          }
+         
          
       }
       catch(Exception e) {System.out.print("Error reading file");}
    }
    
-   //This is the solve method, change the method used if antoher way to solve is found
+   //This is the solve method, change the method used if another way to solve is found
+   //as of right now a jumble method is called
+   //Solve takes the input, jumbles it, and checks if the resulting string is in the list of all words
    
    public static String solve(String input) //try a unique id to each word -- maybe multiplication
    {
       boolean done = false;
       String jumble = "";
-      for(int k = 0; k < listWords.size(); k++)
+      while(!done)
       {
-         
-         if(!done)
-         {
-            jumble = jumble(input);
-            System.out.println(jumble);
-         }
-         else
-            break;
+         count++;
+         jumble = jumble(input);
+         System.out.println(jumble);
          for(String str: listWords)
          {
             if(jumble.equals(str))
@@ -56,16 +85,25 @@ public class Dejumbler
       return jumble;
    }
    
-   public static String jumble(String in) //try random every letter -- longer but would eventually work
+   //randomly jumbles the word --> does not check for previously created jumbles (created more lag when check created)
+   public static String jumble(String in) 
    {
       
       int length = in.length();
-      int[] exclude = new int[length];
+      
       boolean ex = false;
       String out = "";
-      ArrayList<String> array = new ArrayList<String>();
       
-      //convert String into String array to allow for easier manipulation
+      //jumble word
+      for(int i = 0; i < length; i++)
+      {
+         int rand = (int)(Math.random()*array.size());
+         
+         String letter = array.get(rand);
+         array.remove(rand);
+         out += letter;
+      }
+       //re-adds all letters back to the array
       for(int k = 0; k < length; k++)
       {
          if(k==length-1)
@@ -74,19 +112,26 @@ public class Dejumbler
             array.add(in.substring(k,k+1));
          
       }
-      for(int i = 0; i < length; i++)
-      {
-         int rand = (int)(Math.random()*array.size());
-         String letter = array.get(rand);
-         array.remove(rand);
-         out += letter;
-      }
-         
+     
       return out;
    }
+   
+   
    public static String getFirstLetter(String input)
    {
       return input.substring(0,1);
-   }         
+   }  
+   
+   public static int countVowels(String input)
+   {
+      int count = 0;
+      for(int k = 0; k < input.length(); k++)
+      {
+         String sub = input.substring(k,k+1);
+         if(sub.equals("a")||sub.equals("e")||sub.equals("i")||sub.equals("o")||sub.equals("u"))
+            count++;
+      }
+      return count;
+   }      
                
 }
